0f72862be28add26b5ee93d14b7b4dc4d370eb7b
celiapaivab <celiadepaivabruno@gmail.com>
2025-09-29 20:41:29 -0300

    Library .env added

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require('dotenv').config()
 
 describe('Transferências', () =>{
   describe('POST /transferencias', () =>{
     it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () =>{
       // Capturar o token
-      const responseLogin = await request('http://localhost:3000')
+      const responseLogin = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
         .send({
@@ -14,7 +15,7 @@ describe('Transferências', () =>{
               })
       const token = responseLogin.body.token;
       
-      const response = await request('http://localhost:3000')
+      const response = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
